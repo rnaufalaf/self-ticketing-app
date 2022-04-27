b5d80b9c28d1e495b2fb8a6bc953c969ef0d210c
Rakha Naufal <rnaufalaf@gmail.com>
2022-04-27 16:44:06 +0700

    nodemon removed

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +10,10 @@ app.use(express.static(__dirname + "/public"));
 const routes = require("./routes");
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
