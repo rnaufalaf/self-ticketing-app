7a70faae60474b728e8d51c893578b545d6684f4
Rakha Naufal <rnaufalaf@gmail.com>
2022-04-28 23:38:05 +0700

    refine

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,10 +11,6 @@ app.use(express.static(__dirname + "/public"));
 const routes = require("./routes");
 app.use(routes);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
 });
